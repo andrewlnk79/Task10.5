4d2017fb072e0beeba6d5a20a9ebef726eac631c
Andrey Linnik <av.linnik79@ya.ru>
2023-04-08 12:39:25 +0300

    Добавьте файлы проекта.

---
+++ Task10.5/ICalc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._5
+{
+    internal interface ICalc
+    {
+        void Calc(double x, double y);  
+    }
+}
